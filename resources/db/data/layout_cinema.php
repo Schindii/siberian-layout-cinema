c95c996fd01f2d10cc5b46adfc3504b070b2a17c
Alex-Yves CREPIN <a.crepin@xtraball.com>
2018-04-06 17:06:57 +0200

    fix: layout for various device sizes, vh/vw issue and background colors.

--- resources/db/data/layout_cinema.php
+++ resources/db/data/layout_cinema.php
@@ -1,24 +1,25 @@
 <?php
-# LayoutCinema
-$default_options = Siberian_Json::encode(array(
-    "menuposition" => 10,
-    "positionleft" => "right",
-    "textTransform" => "title-lowcase",
-    "title" => "titlehidden",
-    "icon" => "iconvisible",
-    "height" => 50,
-    "padding" => 0,
-    "lineheight" => 50,
-    "fontsize" => 28,
-));
+// LayoutCinema!
+$defaultOptions = Siberian_Json::encode(
+    [
+        'menuposition' => 10,
+        'positionleft' => 'right',
+        'textTransform' => 'title-lowcase',
+        'title' => 'titlevisible',
+        'icon' => 'iconvisible',
+        'height' => 50,
+        'padding' => 0,
+        'lineheight' => 50,
+        'fontsize' => 28,
+    ]
+);
 
-$layout_category = new Application_Model_Layout_Category();
-$default_layout_category = $layout_category->find("default", "code");
+$defaultLayoutCategory = (new Application_Model_Layout_Category())
+    ->find('default', 'code');
 
-# LayoutCinema
-$datas = array(
+$datas = [
     'name' => 'Layout Cinema',
-    'category_id' => $default_layout_category->getId(),
+    'category_id' => $defaultLayoutCategory->getId(),
     'visibility' => Application_Model_Layout_Homepage::VISIBILITY_HOMEPAGE,
     'code' => 'layout_cinema',
     'preview' => '/customization/layout/homepage/layout_cinema.png',
@@ -26,13 +27,13 @@ $datas = array(
     'use_more_button' => 0,
     'use_horizontal_scroll' => 0,
     'number_of_displayed_icons' => 4,
-    'position' => "bottom",
-    "order" => 1,
-    "can_uninstall" => 1,
-    "is_active" => 1,
-    "use_subtitle" => 1,
-    "use_homepage_slider" => 0,
-    "options" => $default_options,
-);
+    'position' => 'bottom',
+    'order' => 1,
+    'can_uninstall' => 1,
+    'is_active' => 1,
+    'use_subtitle' => 1,
+    'use_homepage_slider' => 0,
+    'options' => $defaultOptions,
+];
 
 Siberian_Feature::installApplicationLayout($datas);
--- resources/var/apps/modules/layout/home/layout_cinema/hooks.js
+++ resources/var/apps/modules/layout/home/layout_cinema/hooks.js
@@ -1,2 +1,50 @@
-angular.module('starter').service('layout_cinema', function ($rootScope, HomepageLayout) {var service = {};service.getTemplate = function() {return "modules/layout/home/layout_cinema/view.html";};service.getModalTemplate = function() {};service.onResize = function() {};service.features = function (features, more_button) {return features;};return service;});
+App.service('layout_cinema', function ($rootScope, HomepageLayout) {
+
+    var service = {};
+
+    /**
+     * Must return a valid template
+     *
+     * @returns {string}
+     */
+    service.getTemplate = function() {
+        return "modules/layout/home/layout_cinema/view.html";
+    };
+
+    /**
+     * Must return a valid template
+     *
+     * @returns {string}
+     */
+    service.getModalTemplate = function() {
+        /*return "templates/home/l10/modal.html";*/
+    };
+
+    /**
+     * onResize is used for css/js callbacks when orientation change
+     */
+    service.onResize = function() {
+        /** Do nothing for this particular one */
+    };
+
+    /**
+     * Manipulate the features objects
+     *
+     * Examples:
+     * - you can re-order features
+     * - you can push/place the "more_button"
+     *
+     * @param features
+     * @param more_button
+     * @returns {*}
+     */
+    service.features = function(features, more_button) {
+        /** Place more button at the end */
+        /**features.overview.options.push(more_button);*/
+        return features;
+    };
+
+    return service;
+
+});
 
